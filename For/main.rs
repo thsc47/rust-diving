fc11cd6a4b4449141001ca96f7ac1117fc4464e9
thsc47 <thsc47@gmail.com>
2023-12-25 13:10:53 -0300

    For

--- For/main.rs
+++ For/main.rs
@@ -1,15 +1,23 @@
 fn main() {
     repeticao1();
+    repeticao2();
 }
 
 fn repeticao1() {
     let multiplicador:u8 = 5;
     let mut contador = 0;
 
-    
+
     while contador > 10 {
         contador += 1;
         println!("{} x {} = {}", multiplicador, contador, multiplicador*contador);
     }
-
 }
+
+fn repeticao2() {
+        let multiplicador:u8 = 5;
+    
+        for i in 10  {
+            println!("{} x {} = {}", multiplicador, i, multiplicador*i);
+        }    
+    }
